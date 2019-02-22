77f5bab8529dd0f7bb3fbf8083019f552a6e9819
hjk5623 <hjk5623@naver.com>
2019-02-22 14:23:34 +0900

    listStage의 TableView와 연결된 ModelClass(권수, 이미지)

---
+++ Model/SubBook.java
@@ -0,0 +1,30 @@
+package Model;
+
+public class SubBook {
+	private String list;
+	private String subImage;
+	
+	public SubBook(String list, String subImage) {
+		super();
+		this.list = list;
+		this.subImage = subImage;
+	}
+
+	public String getList() {
+		return list;
+	}
+
+	public void setList(String list) {
+		this.list = list;
+	}
+
+	public String getSubImage() {
+		return subImage;
+	}
+
+	public void setSubImage(String subImage) {
+		this.subImage = subImage;
+	}
+
+	
+}
